fc265a66262c28ca08e77eeb1fd74c816928e316
EmeraldTP <68008939+EmeraldTP@users.noreply.github.com>
2022-07-12 15:13:51 +0300

    Add files via upload

--- durak.cpp
+++ durak.cpp
@@ -1,28 +1,82 @@
 #include <iostream>
+#include <algorithm> 
 #include "table.h"
+#include <array>        
+#include <random>       
+#include <chrono>
 using namespace std;
+
+
+
+class Logic
+{
+  private:
+  
+  int * kartid;
+  public:
+  int nrkarta;
+  std::array<int,36> kartidsh ;
+
+      void Lshuffle() {
+      for (int i = 0; i <= 35; i++){
+        kartidsh[i] = i+1;
+        //cout << kartidsh[i];
+      }
+      cout << "\n";
+     //(rand() % 36) +1
+      unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+      shuffle (kartidsh.begin(), kartidsh.end(), default_random_engine(seed));
+    }
+
+    void Player1(){
+
+    }
+    void Player2(){
+
+    }
+    void kaloda(){
+
+    }
+};
 
 class Grafic
 {
   public:
 
-    void display() {
-      cout << "Hello World\n";
-    }
-};
-
-class Logic
-{
-  public:
-
-    void display1() {
+    void Player1(){
+      Logic Logic;
       Table Table;
-      cout << Table.kart(1) <<"\n";
+      Logic.nrkarta =1;
+      cout << Table.kart(Logic.kartidsh[1]);
+    }
+    void Player2(){
+
+    }
+    void kaloda(){
+
+    }
+
+    
+    void k36ut(){
+      Table Table;
+      for (int i = 1; i <= 36; i++){
+      cout << Table.kart(i) <<"\n";}
+    }
+    
+    void display() {
+      Logic Logic;
+      system("clear");
+      Logic.Lshuffle();
+       for (int i = 1; i <= 36; i++){
+         //cout << Logic.kartidsh[i-1] << " ";
+      Player1();
+      }
     }
 };
+
 int main() {
     Grafic t;
     t.display();
     Logic t1;
-    t1.display1();
+    
 }
--- table.h
+++ table.h
@@ -6,22 +6,86 @@ class Table
      int kid;
      char *returm;
      string returmst;
-     string pi = "Pika";
-     string k = "krest";
-     string bu = "bubna";
-     string kr = "krasnia";
+     string pi = "_Pika";
+     string k = "_krest";
+     string bu = "_bubna";
+     string kr = "_krasnia";
   public:
     char* kart(int kid) {
 
       if (kid == 1)
         returmst= "6" + pi;
       else if (kid == 2)
-        returmst = "6" + k;
+        returmst = "7" + pi;
       else if (kid == 3)
-        returmst = "6" + bu;
+        returmst = "8" + pi;
       else if (kid == 4)
+        returmst = "9" + pi;
+      else if (kid == 5)
+        returmst = "10" + pi;
+      else if (kid == 6)
+        returmst = "J" + pi;
+      else if (kid == 7)
+        returmst = "Q" + pi;
+      else if (kid == 8)
+        returmst = "K" + pi;
+      else if (kid == 9)
+        returmst = "A" + pi;
+      else if (kid == 10)
+        returmst = "6" + k;
+      else if (kid == 11)
+        returmst = "7" + k;
+      else if (kid == 12)
+        returmst = "8" + k;
+      else if (kid == 13)
+        returmst = "9" + k;
+      else if (kid == 14)
+        returmst = "10" + k;
+      else if (kid == 15)
+        returmst = "J" + k;
+      else if (kid == 16)
+        returmst = "Q" + k;
+      else if (kid == 17)
+        returmst = "K" + k;
+      else if (kid == 18)
+        returmst = "A" + k;
+      else if (kid == 19)
+        returmst = "6" + bu;
+      else if (kid == 20)
+        returmst = "7" + bu;
+      else if (kid == 21)
+        returmst = "8" + bu;
+      else if (kid == 22)
+        returmst = "9" + bu;
+      else if (kid == 23)
+        returmst = "10" + bu;
+      else if (kid == 24)
+        returmst = "J" + bu;
+      else if (kid == 25)
+        returmst = "Q" + bu;
+      else if (kid == 26)
+        returmst = "K" + bu;
+      else if (kid == 27)
+        returmst = "A" + bu;
+      else if (kid == 28)
         returmst = "6" + kr;
-      
+      else if (kid == 29)
+        returmst = "7" + kr;
+      else if (kid == 30)
+        returmst = "8" + kr;
+      else if (kid == 31)
+        returmst = "9" + kr;
+      else if (kid == 32)
+        returmst = "10" + kr;
+      else if (kid == 33)
+        returmst = "J" + kr;
+      else if (kid == 34)
+        returmst = "Q" + kr;
+      else if (kid == 35)
+        returmst = "K" + kr;
+      else if (kid == 36)
+        returmst = "A" + kr;
+
       //returmst = "Table\n";
       returm = &returmst[0];
       return returm;
