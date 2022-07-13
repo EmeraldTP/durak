c6947ad9e884a3007d7d75c4033965a995978f68
EmeraldTP <68008939+EmeraldTP@users.noreply.github.com>
2022-07-13 15:07:39 +0300

    Add files via upload

--- durak.cpp
+++ durak.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
-#include <algorithm> 
 #include "table.h"
-#include <array>        
-#include <random>       
-#include <chrono>
 using namespace std;
 
-
-
+int kartid[36];
+int nrkarta =1;
 class Logic
 {
   private:
   
-  int * kartid;
   public:
-  int nrkarta;
-  std::array<int,36> kartidsh ;
-
+  
+  
       void Lshuffle() {
-      for (int i = 0; i <= 35; i++){
-        kartidsh[i] = i+1;
+      int kartidsh[36];
+    for (int i = 0; i <= 35; i++){
+        kartidsh[i+1] = i+1;
         //cout << kartidsh[i];
       }
-      cout << "\n";
-     //(rand() % 36) +1
-      unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-      shuffle (kartidsh.begin(), kartidsh.end(), default_random_engine(seed));
+    // for (int i =1;i<=36;i++){
+    //   cout << kartidsh[i]<< " " ; 
+    // }
+    // cout << "\n";
+
+    srand(time(0));
+    int id;
+    for (int i =1; i<=36;i++){
+        id = (rand() % 36) +1;
+        for (int ii=1; ii<=36;ii++){
+            
+            if (kartidsh[id] == 0){id++;}
+            if (id == 37){id =1;}
+        }
+        kartid[i] = kartidsh[id];
+        kartidsh[id] =0;
+    }
+    // for (int i =1;i<=36;i++){
+    //   cout << kartid[i]<< " " ; 
+    // }
+    // cout << "\n";
     }
 
     void Player1(){
@@ -44,17 +56,35 @@ class Grafic
   public:
 
     void Player1(){
+      
       Logic Logic;
       Table Table;
-      Logic.nrkarta =1;
-      cout << Table.kart(Logic.kartidsh[1]);
-    }
-    void Player2(){
-
+      cout << "                    ";
+      for (int i = 1;i<=6; i++){
+        nrkarta++;
+        if ( i == 4){cout << "                    ";}
+        cout << Table.kart(kartid[nrkarta])<< " ";
+        if ( i == 3){cout << "\n";}}
+      cout << "\n";
     }
     void kaloda(){
-
+      Table Table;
+      nrkarta++;
+      cout <<"\n\n\n" <<Table.kart(kartid[nrkarta])<< "           ============================================" << "\n\n\n\n";
     }
+
+    void Player2(){
+      Logic Logic;
+      Table Table;
+      cout << "                    ";
+      for (int i = 1;i<=6; i++){
+        nrkarta++;
+        if ( i == 4){cout << "                    ";}
+        cout << Table.kart(kartid[nrkarta])<< " ";
+        if ( i == 3){cout << "\n";}}
+      cout << "\n";
+    }
+   
 
     
     void k36ut(){
@@ -69,14 +99,18 @@ class Grafic
       Logic.Lshuffle();
        for (int i = 1; i <= 36; i++){
          //cout << Logic.kartidsh[i-1] << " ";
-      Player1();
       }
+      Player1();
+      kaloda();
+      Player2();
     }
 };
 
 int main() {
+   Logic t1;
     Grafic t;
+   // t1.Lshuffle();
     t.display();
-    Logic t1;
+   
     
 }
