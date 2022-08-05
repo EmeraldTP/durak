f465ac446be3f100236e0ee878847d575ff06f93
EmeraldTP <68008939+EmeraldTP@users.noreply.github.com>
2022-08-05 22:46:40 +0300

    Add files via upload

--- durak.cpp
+++ durak.cpp
@@ -8,50 +8,130 @@ class Logic
 {
   private:
   int nrkarta =1;
-
-  
-  public:
   int kartid[36];
   
+  public:
+  int Player1kart;
+  int Player2kart;
   
-      void Lshuffle() {
+    void Lshuffle() {
       int kartidsh[36];
-    for (int i = 0; i <= 35; i++){
-        kartidsh[i+1] = i+1;
-        //cout << kartidsh[i];
-      }
-    // for (int i =1;i<=36;i++){
-    //   cout << kartidsh[i]<< " " ; 
-    // }
-    // cout << "\n";
-
-    srand(time(0));
-    int id;
-    for (int i =1; i<=36;i++){
-        id = (rand() % 36) +1;
-        for (int ii=1; ii<=36;ii++){
-            
-            if (kartidsh[id] == 0){id++;}
-            if (id == 37){id =1;}
+      for (int i = 0; i <= 35; i++){
+          kartidsh[i+1] = i+1;
+          //cout << kartidsh[i];
         }
-        kartid[i] = kartidsh[id];
-        kartidsh[id] =0;
-    }
-    // for (int i =1;i<=36;i++){
-    //   cout << kartid[i]<< " " ; 
-    // }
-    // cout << "\n";
+      // for (int i =1;i<=36;i++){
+      //   cout << kartidsh[i]<< " " ; 
+      // }
+      // cout << "\n";
+
+      srand(time(0));
+      int id;
+      for (int i =1; i<=36;i++){
+          id = (rand() % 36) +1;
+          for (int ii=1; ii<=36;ii++){
+              
+              if (kartidsh[id] == 0){id++;}
+              if (id == 37){id =1;}
+          }
+          kartid[i] = kartidsh[id];
+          kartidsh[id] =0;
+      }
+      // for (int i =1;i<=36;i++){
+      //   cout << kartid[i]<< " " ; 
+      // }
+      // cout << "\n";
     }
 
-    string Player1G(){
-      string pl1textout;
-       Table Table;
-      pl1textout =pl1textout + "                    ";
-      for (int i = 1;i<=6; i++){
+    int * Player1kartAdd(int Player1kartF[37]){
+      // int Player1kartF[36];
+      // for (int i = 1;i<=36;i++){
+      //   Player1kartF[i]=Player1kartT[i];
+      // }
+      int NrKartPlayer1 =0;
+      // for (int i=1;i<=36;i++){
+      //   cout<<Player1kartF[i];
+      // }
+      // cout<<"\n";
+      
+
+
+      //Сартеровка
+      //int size = sizeof( Player1kartF ) / sizeof( Player1kartF[0] );
+      int size = 37;
+      for (int i =1;i<= size; i++){
+        if (Player1kartF[i] ==0){
+          Player1kartF[i]= 99;
+        }
+      }
+
+      int temp;
+      for (int i = 1; i <= size; i++) {
+        for (int j = 1; j <= size - i; j++) {
+            if (Player1kartF[j] > Player1kartF[j + 1]) {
+                // меняем элементы местами
+                temp = Player1kartF[j];
+                Player1kartF[j] = Player1kartF[j + 1];
+                Player1kartF[j + 1] = temp;
+            }
+        }
+      }
+      for (int i=1;i<=36;i++){
+        if (Player1kartF[i] != 99){
+          NrKartPlayer1 = i;
+          break;
+        }
+      }
+      //cout<< NrKartPlayer1;
+      if (NrKartPlayer1 <= 6){
+        //cout<< ":(" << NrKartPlayer1;
+        for(int i = NrKartPlayer1+1;i <=6; i++){
+        //cout <<nrkarta;
+        Player1kartF[i]=kartid[nrkarta];
+        //cout << kartid[nrkarta];
         nrkarta++;
-        if ( i == 4){pl1textout =pl1textout + "                    ";}
-        pl1textout =pl1textout +Table.kart(kartid[nrkarta])+ " ";
-        if ( i == 3){pl1textout =pl1textout + "\n";}}
+        }
+        //cout <<"\n";
+      }
+      
+      for (int i = 1; i <= size; i++) {
+        for (int j = 1; j <= size - i; j++) {
+            if (Player1kartF[j] > Player1kartF[j + 1]) {
+                // меняем элементы местами
+                temp = Player1kartF[j];
+                Player1kartF[j] = Player1kartF[j + 1];
+                Player1kartF[j + 1] = temp;
+            }
+        }
+      }
+      // for (int i=1;i<=36;i++){
+      //   cout<<Player1kartF[i]<< " ";
+      // }
+      // cout << "\n";
+      // for (int i = 1;i<=36;i++){
+      //   Player1kartT[i]=Player1kartF[i];
+      // }
+      return Player1kartF; 
+    }
+
+    string Player1G(int KartPlayer1[37]){
+      int Lnrkarta=0;
+      string pl1textout;
+      Table Table;
+      int NrKartPlayer1;
+
+      for (int i=1;i<=36;i++){
+        if (KartPlayer1[i] == 99){
+          NrKartPlayer1 = i-1;
+          break;
+        }
+      }
+      pl1textout =pl1textout + "                    ";
+      for (int i = NrKartPlayer1;i>=1; i--){
+        Lnrkarta++;
+        if ( i == (NrKartPlayer1/2)){pl1textout =pl1textout + "                    ";}
+        pl1textout =pl1textout +Table.kart(KartPlayer1[Lnrkarta])+ " ";
+        if ( i == ((NrKartPlayer1+2)/2)){pl1textout =pl1textout + "\n";}}
       pl1textout =pl1textout + "\n";
       return pl1textout;
     }
@@ -109,15 +189,29 @@ class Grafic
 
 int main() {
   //int nrkarta,int* kartid
+  int KartPlayer1[37];
+  for (int i = 1;i<=36;i++){
+        KartPlayer1[i]=99;
+  }
+  KartPlayer1[37]=99;
+  int KartPlayer2[37];
+  for (int i = 1;i<=36;i++){
+        KartPlayer2[i]=99;
+  }
+  KartPlayer2[37]=99;
+  // for (int i=1;i<=36;i++){
+  //   cout<<KartPlayer1[i];
+  // }
   Logic Logic;
   Grafic Grafic;
   Logic.Lshuffle();
   Grafic.display();
   
-  string player1string = Logic.Player1G();
-  string player2string = Logic.Player2G();
+  string player1string = Logic.Player1G( Logic.Player1kartAdd(KartPlayer1));
+  string player2string = Logic.Player1G( Logic.Player1kartAdd(KartPlayer2));
   string kolodastring = Logic.kalodaG();
   Grafic.Player1(player1string);
   Grafic.kaloda(kolodastring);
   Grafic.Player2(player2string);
+  //Logic.Player1kartAdd(KartPlayer1);
 }
